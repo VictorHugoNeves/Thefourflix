f568cff5392d68d62804e6290a5f502fabce6d87
VictorHugoNeves <victorhna@hotmail.com>
2020-08-19 12:44:39 -0300

    Comitão da massa mesmo

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -29,8 +29,8 @@ function CadastroCategoria() {
   }
 
   useEffect(() => {
-    const URL = 'http://localhost:8080/categorias';
-    fetch('URL')
+    const URL_TOP = 'http://localhost:8080/categorias';
+    fetch(URL_TOP)
       .then(async (respostaDoServidor) => {
         const resposta = await respostaDoServidor.json();
         setCategorias([
